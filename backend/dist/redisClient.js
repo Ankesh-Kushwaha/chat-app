57691d20987ffca509ca16ff7a486fc52159fc91
ankeshkushwaha <ankeshkush9651@gmail.com>
2025-10-27 23:36:53 +0530

    solve some deployement issue

--- backend/dist/redisClient.js
+++ backend/dist/redisClient.js
@@ -1,8 +1,9 @@
 import { createClient } from "redis";
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 export async function main() {
-    const publishClient = createClient();
+    const publishClient = createClient({ url: redisUrl });
     await publishClient.connect();
-    const subscribeClient = createClient();
+    const subscribeClient = createClient({ url: redisUrl });
     await subscribeClient.connect();
     return { publishClient, subscribeClient };
 }
