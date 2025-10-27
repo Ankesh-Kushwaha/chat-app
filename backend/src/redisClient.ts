3884117f3bf9a33b2b21c5cc4f35e03b5d928c4a
ankeshkushwaha <ankeshkush9651@gmail.com>
2025-10-27 23:31:41 +0530

    take a redis instance for the production

--- backend/src/redisClient.ts
+++ backend/src/redisClient.ts
@@ -1,10 +1,11 @@
 import { createClient, type RedisClientType, type RedisDefaultModules } from "redis";
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379"; 
 
 export async function main() {
-  const publishClient = createClient();
+  const publishClient = createClient({url:redisUrl});
   await publishClient.connect();
 
-  const subscribeClient = createClient();
+  const subscribeClient = createClient({url:redisUrl});
   await subscribeClient.connect();
 
   return { publishClient, subscribeClient };
